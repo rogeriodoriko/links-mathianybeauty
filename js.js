a4bf27540819c1e6c56d123c7902511bd4e2b7e2
Rogério Fernandes <112347141+rogeriodoriko@users.noreply.github.com>
2023-06-21 20:12:13 -0300

    mathiany-beauty

--- js.js
+++ js.js
@@ -8,3 +8,13 @@ console.log(x);
 x.addEventListener("click", () => {
   window.location.href = "https://www.instagram.com/mathianybeauty/";
 });
+var x = document.querySelector("#facebook");
+console.log(x);
+x.addEventListener("click", () => {
+  window.location.href = "https://www.facebook.com/mathyamakeup";
+});
+var x = document.querySelector("#tiktok");
+console.log(x);
+x.addEventListener("click", () => {
+  window.location.href = "https://www.tiktok.com/@mathiany_";
+});
