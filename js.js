93589242098b6176fe319eb750568ce373f4eecf
Rogério Fernandes <112347141+rogeriodoriko@users.noreply.github.com>
2023-06-21 19:50:29 -0300

    projetov1mathbeauty

---
+++ js.js
@@ -0,0 +1,10 @@
+var x = document.querySelector("#whats");
+console.log(x);
+x.addEventListener("click", () => {
+  window.location.href = "https://contate.me/mathianybeauty";
+});
+var x = document.querySelector("#insta");
+console.log(x);
+x.addEventListener("click", () => {
+  window.location.href = "https://www.instagram.com/mathianybeauty/";
+});
